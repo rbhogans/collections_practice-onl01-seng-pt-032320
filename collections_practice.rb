6bc8127315fd0e9d1e8fcc2a4eacbbf02d03e858
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-01 05:10:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,6 +6,10 @@ def sort_array_desc(integers)
   integers.sort {|x, y|  y <=> x}
 end
 
+array = [4, 5, 6, 7]
 def sort_array_char_count(integers)
 integers.sort_by {|x| x.length}
+
+array[0], array[3] = array[3], array[0]
+
 end
